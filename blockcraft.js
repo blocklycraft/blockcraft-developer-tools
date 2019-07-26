8f43308e9f89dac0bc7810bd4de8034b73f53fd4
hempflower <1993996310@qq.com>
2019-07-26 22:48:21 +0800

    Init

---
+++ blockcraft.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const args = process.argv;
+const libraryBuilder = require('./libraryBuilder')
+
+console.log("Welcome use BlockCraft developer tools!");
+if(args.length===2){
+    printHelpText();
+}
+if("build"===args[2]){
+    libraryBuilder.build();
+    return;
+}
+if(args.length===4&&"create"===args[2]){
+    libraryBuilder.create(args[3]);
+    return
+}
+printHelpText();
+
+
+function printHelpText(){
+    console.log("Usage: blockcraft <args>")
+    console.log("   build  Build library")
+    console.log("   create <name>  Create an empty library")
+}
---
+++ libraryBuilder.js
@@ -0,0 +1,101 @@
+const uglifyJS = require('uglify-js')
+const fs = require('fs')
+const path = require('path')
+function getFileList(dir, endwith) {
+    let results = []
+    const files = fs.readdirSync(dir)
+    files.forEach(function (item, index) {
+        let stat = fs.lstatSync(dir + '/' + item)
+        if (stat.isDirectory() === false && path.extname(item)) {
+            results.push(item)
+        }
+    })
+    return results;
+}
+function deleteFolderRecursive(path) {
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file) {
+            var curPath = path + "/" + file;
+            if(fs.statSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+module.exports = {
+    build() {
+        console.log("Building library, Please wait....")
+        deleteFolderRecursive(process.cwd()+'/build')
+        fs.mkdirSync(process.cwd()+'/build')
+        fs.mkdirSync(process.cwd()+'/build/output')
+        console.log("Step 1: Make bundle")
+        if(fs.existsSync(process.cwd()+'/src/bundle')){
+            let js_files = getFileList(process.cwd()+'/src/bundle')
+            let result = '';
+            for(let js_file of js_files){
+                let res = uglifyJS.minify(fs.readFileSync(process.cwd()+'/src/bundle/'+js_file, "utf8"),{mangle: false})
+                result += res.code + '\n'
+            }
+            
+            fs.writeFileSync(process.cwd()+'/build/output/bundle.js',result)
+        }else{
+            console.log("Skip make bundle, because ./src/bundle doesn't exists.")
+        }
+        console.log("Step 2: Make blocks.js")
+        if(fs.existsSync(process.cwd()+'/src/blocks')){
+            let js_files = getFileList(process.cwd()+'/src/blocks')
+            let result = '';
+            for(let js_file of js_files){
+                let res = uglifyJS.minify(fs.readFileSync(process.cwd()+'/src/blocks/'+js_file, "utf8"),{mangle: false})
+                result += res.code + '\n'
+            }
+            fs.writeFileSync(process.cwd()+'/build/output/blocks.js',result)
+        }else{
+            console.log("Skip make blocks.js, because ./src/blocks doesn't exists.")
+        }
+        console.log("Step 3: Copy resources")
+        if(fs.existsSync(process.cwd()+'/src/resources')){
+
+            let _files = getFileList(process.cwd()+'/src/resources')
+            for(let file of _files){
+                fs.copyFileSync(process.cwd()+'/src/resources/'+file, process.cwd()+'/build/output/'+file)
+            }
+        }else{
+            console.log("Skip Copy resources, because ./src/resources doesn't exists.")
+        }   
+    },
+    create(name){
+        let re = /^[\u4E00-\u9FA5A-Za-z0-9_\-]+$/
+        if(!re.test(name)){
+            console.log("Unable to create library, because project name invild!")
+            return;
+        }
+        let info = {
+            name:name,
+            author:'',
+            version:''
+        }
+
+        let info_json = JSON.stringify(info, null, "\t");
+        //Create file
+        //mkdir
+        console.log("Step 1:Make dirs");
+        if(fs.existsSync(process.cwd()+'/'+name)&&fs.lstatSync(process.cwd()+'/'+name).isDirectory()){
+            console.log("ERROR:Unable to create library, because the directory already exists!")
+            return;
+        }
+        fs.mkdirSync(process.cwd()+'/'+name);
+        fs.mkdirSync(process.cwd()+'/'+name+'/src');
+        fs.mkdirSync(process.cwd()+'/'+name+'/src/bundle');
+        fs.mkdirSync(process.cwd()+'/'+name+'/src/blocks');
+        fs.mkdirSync(process.cwd()+'/'+name+'/src/resources');
+        //Write info.json
+        console.log("Step 2:Write info.json");
+        fs.writeFileSync(process.cwd()+'/'+name+'/info.json',info_json);
+        console.log("Done!");
+    }
+}
